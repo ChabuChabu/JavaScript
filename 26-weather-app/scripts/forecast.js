43c946c5ce5c0354dadd026467da7be648635844
Chabu Chabu <chabu.chabu@episerver.com>
2023-03-02 21:57:24 +0000

    get weather information

--- 26-weather-app/scripts/forecast.js
+++ 26-weather-app/scripts/forecast.js
@@ -1,8 +1,9 @@
 const key = "0CFyJQlkskesCGcg7TITBQN8YnOaihNG";
+//get weather information
 
-const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const query = `?apikey=${key}&q=${city}`;
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
@@ -10,6 +11,22 @@ const getCity = async (city) => {
   return data[0];
 };
 
-getCity("London")
-  .then((data) => console.log(data))
+//get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  console.log(data[0]);
+  return data[0];
+};
+
+getCity("Lusaka")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
   .catch((err) => console.log(err));
