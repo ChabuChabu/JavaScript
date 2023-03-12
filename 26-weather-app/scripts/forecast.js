4f8f5bc2e6d7dcdb683ff5309f81584253eaa250
Chabu Chabu <chabu.chabu@episerver.com>
2023-03-12 23:23:26 +0000

    get form date

--- 26-weather-app/scripts/forecast.js
+++ 26-weather-app/scripts/forecast.js
@@ -18,15 +18,5 @@ const getCity = async (city) => {
 
   const response = await fetch(base + query);
   const data = await response.json();
-  console.log(data[0]);
   return data[0];
 };
-
-getCity("Lusaka")
-  .then((data) => {
-    return getWeather(data.Key);
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => console.log(err));
